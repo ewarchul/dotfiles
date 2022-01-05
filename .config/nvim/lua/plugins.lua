1208eebee027fe6b798ed07c24ddf81b446b5452
Eryk Warchulski <ewarchul@gmail.com>
2022-01-05 20:58:20 +0100

    Update plugins.lua

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,61 +1,13 @@
--- vim.o.tabline = '%!v:lua.require\'l
--- vim.o.tabline = '%!v:lua.require\'luatab\'.tabline()'
---[[require'lualine'.setup {]] --[[options = {]] --[[theme = 'molokai']] --[[},]] --[[}]] 
 require 'gitsigns'.setup()
 require 'feline'.setup()
 require 'lsp_signature'.setup()
-
-local cmp = require 'cmp'
-
-local saga = require 'lspsaga'
-saga.init_lsp_saga()
-
-
-
-
-cmp.setup({
-    snippet = {
-        expand = function(args)
-            vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-            -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-            -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-            -- require'snippy'.expand_snippet(args.body) -- For `snippy` users.
-        end
-    },
-    mapping = {
-        ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), {'i', 'c'}),
-        ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), {'i', 'c'}),
-        ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), {'i', 'c'}),
-        ['<C-y>'] = cmp.config.disable, -- If you want to remove the default `<C-y>` mapping, You can specify `cmp.config.disable` value.
-        ['<C-e>'] = cmp.mapping({
-            i = cmp.mapping.abort(),
-            c = cmp.mapping.close()
-        }),
-        ['<CR>'] = cmp.mapping.confirm({select = true})
-    },
-    sources = cmp.config.sources({
-        {name = 'nvim_lsp'}
-        -- { name = 'luasnip' }, -- For luasnip users.
-        -- { name = 'ultisnips' }, -- For ultisnips users.
-        -- { name = 'snippy' }, -- For snippy users.
-    }, {{name = 'buffer'}})
-})
-
--- Use buffer source for `/`.
-cmp.setup.cmdline('/', {sources = {{name = 'buffer'}}})
-
--- Use cmdline & path source for ':'.
-cmp.setup.cmdline(':', {
-    sources = cmp.config.sources({{name = 'path'}}, {{name = 'cmdline'}})
-})
-
 require'colorizer'.setup()
 require'nvim-web-devicons'.get_icons()
 require'nvim-treesitter.configs'.setup {
     rainbow = {
         enable = true,
-        extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
-        max_file_lines = 1000 -- Do not enable for files with more than 1000 lines, int
+        extended_mode = true,
+        max_file_lines = 1000,
     }
 }
 require 'trouble'.setup {}
@@ -64,8 +16,36 @@ require'nvim-treesitter.configs'.setup {
     highlight = {enable = true}
 }
 
-local nvim_lsp = require 'lspconfig'
--- require 'lspinstall'.setup()
+local cmp = require 'cmp'
+local saga = require 'lspsaga'
+saga.init_lsp_saga()
+
+cmp.setup({
+    snippet = {
+        expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body)
+        end
+    },
+    mapping = {
+        ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), {'i', 'c'}),
+        ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), {'i', 'c'}),
+        ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), {'i', 'c'}),
+        ['<C-y>'] = cmp.config.disable,
+        ['<C-e>'] = cmp.mapping({
+            i = cmp.mapping.abort(),
+            c = cmp.mapping.close()
+        }),
+        ['<CR>'] = cmp.mapping.confirm({select = true})
+    },
+    sources = cmp.config.sources({
+        {name = 'nvim_lsp'}
+    }, {{name = 'buffer'}})
+})
+
+cmp.setup.cmdline('/', {sources = {{name = 'buffer'}}})
+cmp.setup.cmdline(':', {
+    sources = cmp.config.sources({{name = 'path'}}, {{name = 'cmdline'}})
+})
 
 local on_attach = function(client, bufnr)
     require"lsp_signature".setup()
@@ -78,7 +58,6 @@ local on_attach = function(client, bufnr)
     buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
     local opts = {noremap = true, silent = true}
     buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-    -- buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
     buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
     buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
     buf_set_keymap('n', '<L>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
@@ -107,94 +86,25 @@ local on_attach = function(client, bufnr)
     buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>",
                    opts)
 end
-nvim_lsp.ccls.setup {
-  cmd = { "/home/ewarchul/.local/share/nvim/lsp_servers/ccls/bin/ccls" },
+
+local lsp_installer = require("nvim-lsp-installer")
+
+lsp_installer.on_server_ready(function(server)
+    local opts = {
+      on_attach = on_attach
+    }
+
+    server:setup(opts)
+end)
+
+local nvim_lsp = require("lspconfig")
+
+nvim_lsp.r_language_server.setup {
   on_attach = on_attach
 }
 
-require'lspconfig'.tsserver.setup{
-  on_attach = on_attach
-
-}
-
-nvim_lsp.bashls.setup {
-  cmd = { "/home/ewarchul/.local/share/nvim/lsp_servers/bash/node_modules/.bin/bash-language-server" },
-  on_attach = on_attach
-}
-
-nvim_lsp.cmake.setup {
-  cmd = { "/home/ewarchul/.local/share/nvim/lsp_servers/cmake/venv/bin/cmake-language-server" },
-  on_attach = on_attach
-}
-
-
---local servers = require'nvim-lsp-installer.servers'
---[[for _, lsp in ipairs(servers) do]]
-  --[[nvim_lsp[lsp].setup {]]
-    --[[on_attach = on_attach,]]
-    --[[flags = {]]
-      --[[debounce_text_changes = 150,]]
-    --[[}]]
-  --[[}]]
---[[end]]
-
--- Compe setup
---[[require'compe'.setup {]]
---[[enabled = true;]]
---[[autocomplete = true;]]
---[[debug = false;]]
---[[min_length = 1;]]
---[[preselect = 'enable';]]
---[[throttle_time = 80;]]
---[[source_timeout = 200;]]
---[[incomplete_delay = 400;]]
---[[max_abbr_width = 100;]]
---[[max_kind_width = 100;]]
---[[max_menu_width = 100;]]
---[[documentation = true;]]
---[[source = {]]
---[[path = true;]]
---[[nvim_lsp = true;]]
---[[neorg = true;]]
---[[};]]
---[[}]]
-
-local t = function(str)
-    return vim.api.nvim_replace_termcodes(str, true, true, true)
-end
-
-local check_back_space = function()
-    local col = vim.fn.col('.') - 1
-    if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-        return true
-    else
-        return false
-    end
-end
-
---[[   vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})]]
---[[vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})]]
 vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
 vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
-
-local colors = {
-    bg = '#282c34',
-    yellow = '#fabd2f',
-    cyan = '#008080',
-    darkblue = '#081633',
-    green = '#afd700',
-    orange = '#FF8800',
-    purple = '#5d4d7a',
-    magenta = '#d16d9e',
-    grey = '#c0c0c0',
-    blue = '#0087d7',
-    red = '#ec5f67'
-}
-
-local buffer_not_empty = function()
-    if vim.fn.empty(vim.fn.expand('%:t')) ~= 1 then return true end
-    return false
-end
 
 local parser_configs = require 'nvim-treesitter.parsers'.get_parser_configs()
 
@@ -209,36 +119,33 @@ parser_configs.norg = {
 local cb = require'diffview.config'.diffview_callback
 
 require'diffview'.setup {
-    diff_binaries = false, -- Show diffs for binaries
+    diff_binaries = false,
     file_panel = {
-        position = "left", -- One of 'left', 'right', 'top', 'bottom'
-        width = 35, -- Only applies when position is 'left' or 'right'
-        height = 10 -- Only applies when position is 'top' or 'bottom
-        -- use_icons = true        -- Requires nvim-web-devicons
+        position = "left",
+        width = 35,
+        height = 10
     },
     key_bindings = {
-        disable_defaults = false, -- Disable the default key bindings
-        -- The `view` bindings are active in the diff buffers, only when the current
-        -- tabpage is a Diffview.
+        disable_defaults = false,
         view = {
-            ["<C-tab>"] = cb("select_next_entry"), -- Open the diff for the next file 
-            ["<s-tab>"] = cb("select_prev_entry"), -- Open the diff for the previous file
-            ["<leader>e"] = cb("focus_files"), -- Bring focus to the files panel
-            ["<leader>b"] = cb("toggle_files") -- Toggle the files panel.
+            ["<C-tab>"] = cb("select_next_entry"),
+            ["<s-tab>"] = cb("select_prev_entry"),
+            ["<leader>e"] = cb("focus_files"),
+            ["<leader>b"] = cb("toggle_files")
         },
         file_panel = {
-            ["j"] = cb("next_entry"), -- Bring the cursor to the next file entry
+            ["j"] = cb("next_entry"),
             ["<down>"] = cb("next_entry"),
-            ["k"] = cb("prev_entry"), -- Bring the cursor to the previous file entry.
+            ["k"] = cb("prev_entry"),
             ["<up>"] = cb("prev_entry"),
-            ["<cr>"] = cb("select_entry"), -- Open the diff for the selected entry.
+            ["<cr>"] = cb("select_entry"),
             ["o"] = cb("select_entry"),
             ["<2-LeftMouse>"] = cb("select_entry"),
-            ["-"] = cb("toggle_stage_entry"), -- Stage / unstage the selected entry.
-            ["S"] = cb("stage_all"), -- Stage all entries.
-            ["U"] = cb("unstage_all"), -- Unstage all entries.
-            ["X"] = cb("restore_entry"), -- Restore entry to the state on the left side.
-            ["R"] = cb("refresh_files"), -- Update stats and entries in the file list.
+            ["-"] = cb("toggle_stage_entry"),
+            ["S"] = cb("stage_all"),
+            ["U"] = cb("unstage_all"),
+            ["X"] = cb("restore_entry"),
+            ["R"] = cb("refresh_files"),
             ["<tab>"] = cb("select_next_entry"),
             ["<s-tab>"] = cb("select_prev_entry"),
             ["<leader>e"] = cb("focus_files"),
@@ -248,69 +155,38 @@ require'diffview'.setup {
 }
 
 local tree_cb = require'nvim-tree.config'.nvim_tree_callback
--- following options are the default
+
 require'nvim-tree'.setup {
-    -- disables netrw completely
     disable_netrw = true,
-    -- hijack netrw window on startup
     hijack_netrw = true,
-    -- open the tree when running this setup function
     open_on_setup = true,
-    -- will not open on setup if the filetype is in this list
     ignore_ft_on_setup = {},
-    -- closes neovim automatically when the tree is the last **WINDOW** in the view
     auto_close = false,
-    -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
     open_on_tab = false,
-    -- hijacks new directory buffers when they are opened.
     update_to_buf_dir = {
-        -- enable the feature
         enable = true,
-        -- allow to open the tree if it was previously closed
         auto_open = true
     },
-    -- hijack the cursor in the tree to put it at the start of the filename
     hijack_cursor = false,
-    -- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually)
     update_cwd = false,
-    -- show lsp diagnostics in the signcolumn
---    lsp_diagnostics = false,
-    -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
+
     update_focused_file = {
-        -- enables the feature
         enable = false,
-        -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
-        -- only relevant when `update_focused_file.enable` is true
         update_cwd = false,
-        -- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
-        -- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
         ignore_list = {}
     },
-    -- configuration options for the system open command (`s` in the tree by default)
     system_open = {
-        -- the command to run this, leaving nil should work in most cases
         cmd = nil,
-        -- the command arguments as a list
         args = {}
     },
-
     view = {
-        -- width of the window, can be either a number (columns) or a string in `%`, for left or right side placement
         width = 30,
-        -- height of the window, can be either a number (columns) or a string in `%`, for top or bottom side placement
         height = 30,
-        -- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
         side = 'left',
-        -- if true the tree will resize itself after opening a file
         auto_resize = false,
         mappings = {
-            -- custom only false will merge the list with the default mappings
-            -- if true, it will only use your list to set the mappings
             custom_only = false,
-            -- list of mappings to set on the tree manually
             list = {{key = "t", cb = tree_cb("tabnew")}}
         }
     }
 }
-
-vim.g.rose_pine_disable_italics = true
