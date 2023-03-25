d993c5df64a93fb6a61af22b7daa05e31b604e16
ewarchul <ewarchul@gmail.com>
2023-03-25 12:55:08 +0100

    fix

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,14 +2,12 @@
 require("lazy_bootstrap")
 require("lazy").setup("plugins")
 
-vim.api.nvim_create_autocmd("VimEnter", {
-	command = "set nornu nonu | Neotree toggle",
-})
-vim.api.nvim_create_autocmd("BufEnter", {
-	command = "set rnu nu",
-})
-
 -- load neovim options
 require("core/opts")
 require("core/key_bindings")
 require("core/color")
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  command = "Neotree toggle",
+})
+
